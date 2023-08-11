9f5e1eb42c3feb3503f7c5bf8a2ddd141829d994
Stephane Monier <smonier@jahia.com>
2023-08-11 16:54:02 +0200

    replace space with dash for interests

--- src/main/java/org/jahia/se/modules/meaningCloud/service/impl/RequestMeaningCloudServicesServiceImpl.java
+++ src/main/java/org/jahia/se/modules/meaningCloud/service/impl/RequestMeaningCloudServicesServiceImpl.java
@@ -50,12 +50,12 @@ public class RequestMeaningCloudServicesServiceImpl implements RequestMeaningClo
 
             // Iterate through the JSON array and extract the values
             for (int i = 0; i < tagsJsonArray.length(); i++) {
-                 JSONObject jsonObject = tagsJsonArray.getJSONObject(i);
+                JSONObject jsonObject = tagsJsonArray.getJSONObject(i);
                     // Get the values of the named elements
-                    String[] array = jsonObject.getString("value").split(",\\s*|-\\s*");
-                    for (String item : array) {
-                        tagsJavaArrayList.add(item.trim().replaceAll(" ", "-"));
-                    }
+                String[] array = jsonObject.getString("value").split(",\\s*|-\\s*");
+                for (String item : array) {
+                    tagsJavaArrayList.add(item.trim().replaceAll(" ", "-"));
+                }
             }
             HashSet<String> uniqueSet = new HashSet<>(tagsJavaArrayList);
             // Convert the set back to an array
@@ -82,15 +82,14 @@ public class RequestMeaningCloudServicesServiceImpl implements RequestMeaningClo
                   
             String cleanText = getTextFromNode (path,language);
             JSONArray tagsJsonArray = triggerCategorisationRequest(meaningCloudApikey,cleanText,meaningCloudClassModel);
-            LOGGER.info("Text to analyse: "+cleanText);
             String[] tagsJavaArray = new String[tagsJsonArray.length()];
 
             // Iterate through the JSON array and extract the values
             for (int i = 0; i < tagsJsonArray.length(); i++) {
-                 JSONObject jsonObject = tagsJsonArray.getJSONObject(i);
+                JSONObject jsonObject = tagsJsonArray.getJSONObject(i);
                 // Get the values of the named elements
                 int lastIndex = jsonObject.getString("value").lastIndexOf('>');
-                String interest = jsonObject.getString("value").substring(lastIndex + 1);
+                String interest = jsonObject.getString("value").substring(lastIndex + 1).replaceAll(" ", "-");
                 int interestWeight = (int) jsonObject.getLong("relevance");
                 tagsJavaArray[i] = interest+":"+interestWeight;
                 LOGGER.info("interest: "+tagsJavaArray[i]);
